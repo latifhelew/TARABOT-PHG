6ccd1b1be579c703ef479e0b9dead3507d0c47e5
latifhelew <phglatif@yahoo.com>
2023-08-03 11:46:08 +0200

    Initial commit

---
+++ styles/ContourLinesTARABOT_10_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_ContourLinesTARABOT_10 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "6.5px \'Times New Roman\', sans-serif";
+    var labelFill = "#e2c843";
+    var bufferColor = "#e70cf2";
+    var bufferWidth = 0.4;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if (feature.get("ELEV") !== null) {
+        labelText = String(feature.get("ELEV"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(223,113,45,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
